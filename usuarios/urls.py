b8cca872c7477b96d31972d70f8cdffc84047e5e
palomaalmeida09 <paloma.monteiro@escolar.ifrn.edu.br>
2024-01-03 16:52:32 -0300

    alterações

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from django.contrib.auth.models import User
 from . import views 
+
+app_name = 'usuarios'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('cadastro/', views.cadastro, name='cadastro'),
